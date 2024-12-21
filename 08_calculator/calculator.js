20e1fc2b889af31e6d5bde916c04f3a34fbefe41
Light170 <yonatanssj@gmail.com>
2024-12-21 17:04:14 +0200

    Commit calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
+};
+
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, num) => total + num, 0);
 };
 
-const sum = function() {
-	
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current, 1);
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  let product = 1;
+  for (let i = 1; i <= n; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
