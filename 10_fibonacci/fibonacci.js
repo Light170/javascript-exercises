d3b0f862c6a8effcc244bce2bc6524129d3d4491
Light170 <yonatanssj@gmail.com>
2024-12-22 23:59:20 +0200

    Commit fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(member) { 
+    member = Number(member);
+    if (isNaN(member)) {
+        return "Invalid input"; 
+    } 
+    
+    if (member === 0) {
+        return 0; 
+    } else if (member < 0) {
+        return "OOPS";
+    }
 
+    let a = 1,
+        b = 1;
+
+    for (let i = 2; i <= member; i++) {  
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return a;
 };
+    
+
+;
 
 // Do not edit below this line
 module.exports = fibonacci;
